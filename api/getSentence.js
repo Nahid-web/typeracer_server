c8c12b71f160a5c91a53aac75052871c8c2c13b3
Nahid-web <nahidamin266@gmail.com>
2024-07-08 23:19:31 +0600

    done

---
+++ api/getSentence.js
@@ -0,0 +1,9 @@
+const axios = require("axios");
+
+const getSentence = async () => {
+  const jokeData = await axios.get("https://api.chucknorris.io/jokes/random");
+
+  return jokeData.data.value.split(" ");
+};
+
+module.exports = getSentence;
